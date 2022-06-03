33f555e62a667e24084ecb18144cf4f01abf2a34
adam <adam.poulton5@gmail.com>
2022-06-03 20:56:08 +1000

    updated nodemon environmental variables, routes and app

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import express, { Express } from "express"
+import mongoose, { ConnectOptions } from "mongoose"
+import cors from "cors"
+import todoRoutes from "./routes"
+
+const app: Express = express()
+
+const PORT: string | number = process.env.PORT || 4000
+
+app.use(cors())
+app.use(todoRoutes)
+
+const uri: string = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.rxg52.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+const options = { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }
+
+
+mongoose
+    .connect(uri, options as ConnectOptions)
+    .then(() => 
+        app.listen(PORT, () =>
+            console.log(`Server running on http://localhost:${PORT}`)
+        )
+    )
+    .catch(error => {
+        throw error
+    })
